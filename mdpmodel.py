54568393ddfabb7bb0f2769f77af872c46f6a1b2
Thomas Ballinger <thomasballinger@gmail.com>
2015-02-25 08:43:55 -0500

    prettier for loops

--- mdpmodel.py
+++ mdpmodel.py
@@ -4,16 +4,16 @@ from __future__ import division
 def model(num_states, num_actions, obs):
     """Returns propability transition matrix and vector of rewards by state"""
     # initialize counters for reward and probability transition matrix
-    s = num_states
-    a = num_actions
-    pcount = [[[0]*s for _ in range(a)] for _ in range(s)]
-    rcount = [[0, 0] for _ in range(s)]
+    pcount = [[[0]*num_states
+               for _ in range(num_actions)]
+              for _ in range(num_states)]
+    rcount = [[0, 0] for _ in range(num_states)]
 
     # count state and reward observations
-    for o in range(0, len(obs)):
-        for t in range(0, len(obs[o])):
+    for ob in obs:
+        for step in ob:
             # REWARD TRACKER:
-            state, action, reward = obs[o][t]
+            state, action, reward = step
 
             # increment cumulative reward for observed state
             rcount[state][0] += reward
@@ -22,21 +22,20 @@ def model(num_states, num_actions, obs):
 
             # PROBABILITY TRANSITION TRACKER
             # increment count of transitions, C_sa[s']
-            if (t < (len(obs[o])-1)):
-                state_ = obs[o][t+1][0]
-                pcount[state][action][state_] += 1
+        for (state, action, _), (next_state, _, _) in zip(ob[:-1], ob[1:]):
+            pcount[state][action][next_state] += 1
 
     # compute R[s]
-    R = [0]*s
-    for i in range(0, s):
+    R = [0]*num_states
+    for i in range(num_states):
         R[i] = rcount[i][0]/rcount[i][1] if (rcount[i][1]) else (0)
 
-    P = [[[0]*s for _ in range(a)] for _ in range(s)]
+    P = [[[0]*num_states for _ in range(num_actions)] for _ in range(num_states)]
     # compute P_sa[s']
-    for i in range(0, s):
-        for j in range(0, a):
+    for i in range(0, num_states):
+        for j in range(0, num_actions):
             visits = sum(pcount[i][j])
-            for k in range(0, s):
-                P[i][j][k] = pcount[i][j][k]/visits if (visits) else (1/s)
+            for k in range(0, num_states):
+                P[i][j][k] = pcount[i][j][k]/visits if (visits) else (1/num_states)
 
     return [P, R]
